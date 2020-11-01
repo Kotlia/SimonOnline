f7ba47666904dbebba45f310703d4d7e8d526542
Kotlia <62182668+Kotlia@users.noreply.github.com>
2020-10-31 21:59:11 -0400

    Swap to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: `./src/js/Main.js`,
+    entry: `./dist/js/Main.js`,
   
     // ファイルの出力設定
     output: {
       //  出力ファイルのディレクトリ名
       path: `${__dirname}/public`,
       // 出力ファイル名
-      filename: "main.js"
+      filename: "bundle.js"
     }
   };
